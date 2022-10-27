bf57f4fcd7602e1bb61f531fbe661ad99f4a244c
AgnesMuita <wamwithamuita@gmail.com>
2022-10-27 13:47:34 +0300

    add twitter and slack user names to profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,8 +3,10 @@ import profile from "../static/blank-profile-picture-png.webp"
 
 function Profile() {
   return (
-    <div class="profile-img">
-      <img src={profile} alt=""/>
+    <div className='profile-section'>
+        <img className="profile-img" src={profile} alt=""/>
+        <h1 className="twitter-frame"><p id="twitter">Agnes Muita</p></h1>
+        {/* <h1 className="twitter-frame"><p id="slack">Agnes Muita</p></h1> */}
     </div>
   )
 }
