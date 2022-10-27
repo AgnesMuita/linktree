9e3a6caca438fb63cfac3e94238ba4bfdf7a4c66
AgnesMuita <wamwithamuita@gmail.com>
2022-10-27 11:19:46 +0300

    fix styling for links

--- src/components/links.js
+++ src/components/links.js
@@ -3,8 +3,20 @@ import React from 'react'
 function Links() {
   return (
     <div className='link-section'>
-      <div class="Links"><frame className="frame5"><text className="text">Links</text></frame></div>
-      <a href='https://training.zuri.team/' type="button" id="btn_zuri">Zuri</a>
+      <div className="Links">
+        <frame className="frame5">
+          <text className="links-text">
+              Links
+          </text>
+          </frame>
+      </div>
+      <a href='https://training.zuri.team/' type="button" id="btn_zuri">
+        <frame className="frame5">
+          <text className="text-zuri">
+            Zuri
+          </text>
+        </frame>
+      </a>
     </div>
   )
 }
