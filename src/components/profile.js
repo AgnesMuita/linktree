7631b3079f2b384a51137666cf63e9a545947174
AgnesMuita <wamwithamuita@gmail.com>
2022-10-27 14:02:11 +0300

    update profile image

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import profile from "../static/blank-profile-picture-png.webp"
+import profile from "../static/profile-img.jpg"
 
 function Profile() {
   return (
     <div className='profile-section'>
         <img className="profile-img" src={profile} alt=""/>
-        <h1 className="twitter-frame"><p id="twitter">Agnes Muita</p></h1>
+        <h1 className="twitter-frame"><p id="twitter">Aggie Muita</p></h1>
         {/* <h1 className="twitter-frame"><p id="slack">Agnes Muita</p></h1> */}
     </div>
   )
