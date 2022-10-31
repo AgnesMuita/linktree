5bed0f87ab9c3736e467dd2a2e928f7f6ba02423
AgnesMuita <wamwithamuita@gmail.com>
2022-10-31 11:51:01 +0300

    update profile names

--- src/components/ProfileSection/ProfileSection.js
+++ src/components/ProfileSection/ProfileSection.js
@@ -5,9 +5,9 @@ import profile from "../../static/profile-img.jpg"
 function Profile() {
   return (
     <div className='profile-section'>
-        <img className="profile-img" src={profile} alt=""/>
-        <h1 className="twitter-frame">Aggie Muita</h1>
-        {/* <h1 className="twitter-frame"><p id="slack">Agnes Muita</p></h1> */}
+        <img className="profile-img" src={profile} alt="" id="profile_img"/>
+        <h1 className="twitter-frame" id="twitter">Aggie Muita</h1>
+        <h1 className="slack-frame"><p id="slack">AggieMuita</p></h1>
     </div>
   )
 }
